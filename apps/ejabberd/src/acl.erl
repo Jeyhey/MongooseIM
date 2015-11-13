7c16f13f3512afea1ee1f9ef733262467e03d1f5
Pawel Pikula <pawel.pikula@erlang-solutions.com>
2015-11-13 11:35:13 +0100

    remove dead code and test some edge cases

--- apps/ejabberd/src/acl.erl
+++ apps/ejabberd/src/acl.erl
@@ -30,10 +30,7 @@
 -export([start/0,
          to_record/3,
          add/3,
-         add_list/3,
-         match_rule/3,
-         % for debugging only
-         match_acl/3]).
+         match_rule/3]).
 
 -include("ejabberd.hrl").
 
@@ -87,35 +84,6 @@ add(Host, ACLName, ACLSpec) ->
                                   aclspec = normalize_spec(ACLSpec)})
         end,
     mnesia:transaction(F).
-
-add_list(Host, ACLs, Clear) ->
-    F = fun() ->
-                if
-                    Clear ->
-                        Ks = mnesia:select(
-                               acl, [{{acl, {'$1', Host}, '$2'}, [], ['$1']}]),
-                        lists:foreach(fun(K) ->
-                                              mnesia:delete({acl, {K, Host}})
-                                      end, Ks);
-                    true ->
-                        ok
-                end,
-                lists:foreach(fun(ACL) ->
-                                      case ACL of
-                                          #acl{aclname = ACLName,
-                                               aclspec = ACLSpec} ->
-                                              mnesia:write(
-                                                #acl{aclname = {ACLName, Host},
-                                                     aclspec = normalize_spec(ACLSpec)})
-                                      end
-                              end, ACLs)
-        end,
-    case mnesia:transaction(F) of
-        {atomic, _} ->
-            ok;
-        _ ->
-            false
-    end.
 
 -spec normalize(_) -> 'error' | binary() | tuple().
 normalize(A) when is_list(A) ->
